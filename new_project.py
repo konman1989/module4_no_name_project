301959dc3971111d96b87ed367611c0cb341bebd
extraname <zorraider@gmail.com>
2019-11-25 13:39:23 +0200

    implemented jpegDownloader

--- new_project.py
+++ new_project.py
@@ -7,11 +7,10 @@ parser.add_argument("-u", metavar="u", type=str, help='link')
 parser.add_argument("-p", metavar="p", type=str, help='path at computer', default='')
 
 args = parser.parse_args()
-print(args)
 
 
 def save_foto(file_name, url, path):
-    with open(f'{path}/{file_name}.jpg', 'wb') as f:
+    with open(f'{path}{file_name}.jpg', 'wb') as f:
         f.write(requests.get(url).content)
 
 
