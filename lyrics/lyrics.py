3a5982e929cc378824104780a180d1a410d2955d
konman1989 <manoilo.blog@gmail.com>
2019-11-28 17:55:14 +0200

    changet to_file check if the song is in DB

--- lyrics/lyrics.py
+++ lyrics/lyrics.py
@@ -41,7 +41,7 @@ def to_file(author, song):
         content = json.load(file)
         for key in content['data']['Songs']:
             if key['artist'] == author and key['name'] == song:
-                if key['lyrics'] != 'text':
+                if len(key['lyrics']) > 10:
                     text_to_download = key['lyrics']
             else:
                 text_to_download = fetch_lyrics(author, song)
@@ -54,9 +54,9 @@ def to_file(author, song):
 
 if __name__ == "__main__":
     to_file('Nirvana', 'In Bloom')
-    to_file('Adele', 'Hello')
-    add_song_text('Adele', 'Hello')
-    add_song_text('Nirvana', 'In Bloom')
+    # to_file('Adele', 'Hello')
+    # add_song_text('Adele', 'Hello')
+    # add_song_text('Nirvana', 'In Bloom')
 
     parser = argparse.ArgumentParser()
 
