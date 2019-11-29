ceb9370b8af8005aef92cf10f757e46878a464d8
konman1989 <manoilo.blog@gmail.com>
2019-11-29 20:08:52 +0200

    add_song method tests implemented

--- tests/tests_lyrics.py
+++ tests/tests_lyrics.py
@@ -1,5 +1,6 @@
 import unittest
-from lyrics.lyrics import fetch_lyrics, to_file
+from lyrics.lyrics import fetch_lyrics, to_file, add_song_text
+from data_base.main import DataBase
 
 
 class TestLyrics(unittest.TestCase):
@@ -10,10 +11,26 @@ class TestLyrics(unittest.TestCase):
 
     def test_to_file_creates_txt_file_and_downloads_content(self):
         text_to_check = fetch_lyrics('Nirvana', 'In Bloom')
-        to_file('Nirvana', 'In Bloom')
+        to_file('Nirvana', 'In Bloom', 'Nirvana_In_Bloom.txt')
 
-        with open('Nirvana_In Bloom.txt', 'r') as file:
-            text_from_file = file.read()
+        with open('Nirvana_In_Bloom.txt', 'r') as file_:
+            text_from_file = file_.read()
 
         self.assertEqual(text_to_check, text_from_file)
 
+    def test_add_song_text_returns_empty_list_if_artist_not_in_db(self):
+        value = add_song_text('Nirvanana', 'In Rotten')
+
+        self.assertIsInstance(value, list)
+
+    def test_add_song_text_adds_text_in_DB_if_missing(self):
+        add_song_text('Nirvana', 'In Bloom')
+        value = DataBase.select('Songs', artist='Nirvana', name='In Bloom')
+        self.assertTrue(len(value[0]['lyrics']) > 10)
+
+        value2 = DataBase.select('Songs', artist='Adele', name='Hello')
+        self.assertTrue(len(value2[0]['lyrics']) < 10)
+
+        add_song_text('Adele', 'Hello')
+        value3 = DataBase.select('Songs', artist='Adele', name='Hello')
+        self.assertTrue(len(value3[0]['lyrics']) > 10)
